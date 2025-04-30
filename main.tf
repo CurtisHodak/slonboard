a684b08363227abfa0e82dff0e979aa4ad713f6a
Curtis Hodak <curtish@spacelift.com>
2025-04-30 15:17:23 -0400

    moving s3 to own file

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
-
-resource "aws_s3_bucket" "sl_on1" {
-  bucket = "ch-slonboard-1"
-
-  tags = {
-    Description = "New bucket to have something around"
-  }
-}
