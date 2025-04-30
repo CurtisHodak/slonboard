e45c021e57d5cd0759d1045940dad3e3908b4516
Curtis Hodak <curtish@spacelift.com>
2025-04-30 12:56:40 -0400

    remove ec2 instance

--- main.tf
+++ main.tf
@@ -2,15 +2,6 @@ resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_instance" "sl_on1" {
-  ami           = "ami-060a84cbcb5c14844" // Amazon linux 
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "First instance just to have something in the repo"
-  }
-}
-
 resource "aws_s3_bucket" "sl_on1" {
   bucket = "ch-slonboard-1"
 
