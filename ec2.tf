017fadf689159be36d4cd135c69221ebba1222b6
Curtis Hodak <curtish@spacelift.com>
2025-05-01 11:51:35 -0400

    new

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "onboarding1" {
   ami           = var.instance_ami
   instance_type = var.instance_type
 }
+
+resource "aws_instance" "onboarding2" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+}
