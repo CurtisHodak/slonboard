5f6266f52fb60a543e8fe7dae5e048e054be4660
Curtis Hodak <curtish@spacelift.com>
2025-05-01 10:14:57 -0400

    add default

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "aws_region" {
-    description = "AWS Region for creating resources"
-    type = string
+  description = "AWS Region for creating resources"
+  type        = string
+  default     = ""
 }
 
 variable "instance_ami" {
-    description = "EC2 AMI to use for new instances"
-    type = string
+  description = "EC2 AMI to use for new instances"
+  type        = string
+  default     = ""
 }
 
 variable "instance_type" {
-    description = "Instance type for the EC2"
-    type = string
+  description = "Instance type for the EC2"
+  type        = string
+  default     = ""
 }
