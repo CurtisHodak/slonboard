1d32b792a7acb918406b75f2fceceacacc534bbf
Curtis Hodak <curtish@spacelift.com>
2025-05-02 11:20:04 -0400

    fix typo

--- chaws/s3.tf
+++ chaws/s3.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "sl_on1" {
 }
 
 module "sl_on2" {
-  source = "spacelift.io/curtishodaks3testmodule"
+  source = "spacelift.io/curtishodak/s3testmodule"
   version = "0.1.0"
 
   bucket = "chslonboard2"
