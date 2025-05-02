abaa5e9d42dc8a0c8c2528840a9446fe26c461cc
Curtis Hodak <curtish@spacelift.com>
2025-05-02 11:37:41 -0400

    change source add aws one more time

--- chaws/s3.tf
+++ chaws/s3.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "sl_on1" {
 }
 
 module "s3testmodule" {
-  source  = "spacelift.io/curtishodak/s3testmodule/spacelift"
+  source  = "spacelift.io/curtishodak/s3testmodule/aws"
   version = "0.1.0"
 
   bucket     = "chslonboard2"
