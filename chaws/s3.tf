73899b31417db06be4bc509184fa80713c9fc63d
Curtis Hodak <curtish@spacelift.com>
2025-05-02 11:55:22 -0400

    Module version

--- chaws/s3.tf
+++ chaws/s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "sl_on1" {
 
 module "s3testmodule" {
   source  = "spacelift.io/curtishodak/s3testmodule/aws"
-  version = "0.1.0"
+  version = "0.1.1"
 
   bucket     = "chslonboard2"
   versioning = true
