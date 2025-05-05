13411226fcdae667f274cec4f2f0319d54a8a9bd
Curtis Hodak <curtish@spacelift.com>
2025-05-05 11:21:19 -0400

    correct volume size

--- chaws/ec2.tf
+++ chaws/ec2.tf
@@ -2,8 +2,11 @@ resource "aws_instance" "onboarding1" {
   ami           = var.instance_ami
   instance_type = var.instance_type
   key_name = "chkey1"
-  volume_size = 25
   vpc_security_group_ids = ["sg-0cbca007e6ac73276"]
+
+  root_block_device {
+    volume_size = 25
+  }
 
   tags = {
     test = "another test"
