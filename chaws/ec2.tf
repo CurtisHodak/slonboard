366f94ea2d918afcaed2b6b4cd12e5014280dc83
Curtis Hodak <curtish@spacelift.com>
2025-05-05 10:49:53 -0400

    delete instance

--- chaws/ec2.tf
+++ chaws/ec2.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "onboarding1" {
-  ami           = var.instance_ami
-  instance_type = var.instance_type
-  key_name = "chkey1"
+# resource "aws_instance" "onboarding1" {
+#   ami           = var.instance_ami
+#   instance_type = var.instance_type
+#   key_name = "chkey1"
 
-  tags = {
-    test = "another test"
-  }
-}
+#   tags = {
+#     test = "another test"
+#   }
+# }
 
 # resource "aws_instance" "onboarding2" {
 #   ami           = var.instance_ami
