1bea49471caa60ea7059bc39a4fd2c8910200637
Curtis Hodak <curtish@spacelift.com>
2025-05-07 13:58:40 -0400

    create new stacks move some items around

--- chaws/variables.tf
+++ chaws/variables.tf
@@ -1,27 +1,3 @@
-variable "aws_region" {
-  description = "AWS Region for creating resources"
-  type        = string
-  default     = ""
-}
-
-variable "instance_ami" {
-  description = "EC2 AMI to use for new instances"
-  type        = string
-  default     = ""
-}
-
-variable "instance_type" {
-  description = "Instance type for the EC2"
-  type        = string
-  default     = ""
-}
-
-variable "volume_size" {
-  description = "Default volume size"
-  type = number
-  default = 5
-}
-
 variable "worker_pool_config" {
   description = "Spacelift Token"
   type        = string
