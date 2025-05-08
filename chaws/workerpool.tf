f95d98970e0413e5b455f8b01ff4cfd1932b07d1
Curtis Hodak <curtish@spacelift.com>
2025-05-08 17:25:39 -0400

    changed things for worker pool

--- chaws/workerpool.tf
+++ chaws/workerpool.tf
@@ -1,18 +1,21 @@
-# module "my_workerpool" {
-#   source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v3.0.3"
+module "my_workerpool" {
+  source = "github.com/spacelift-io/terraform-aws-spacelift-workerpool-on-ec2?ref=v3.0.3"
 
-#   secure_env_vars = {
-#     SPACELIFT_TOKEN            = var.worker_pool_config
-#     SPACELIFT_POOL_PRIVATE_KEY = var.worker_pool_private_key
-#   }
-#   configuration = <<EOF
-#     export SPACELIFT_SENSITIVE_OUTPUT_UPLOAD_ENABLED=true
-#   EOF
+  secure_env_vars = {
+    SPACELIFT_TOKEN            = var.worker_pool_config
+    SPACELIFT_POOL_PRIVATE_KEY = var.worker_pool_private_key
+  }
+  configuration = <<EOF
+    export SPACELIFT_SENSITIVE_OUTPUT_UPLOAD_ENABLED=true
+  EOF
 
-#   ec2_instance_type = "t3.medium"
-#   min_size          = 1
-#   max_size          = 5
-#   worker_pool_id    = var.worker_pool_id
-#   security_groups   = var.worker_pool_security_groups
-#   vpc_subnets       = var.worker_pool_subnets
-# }
+  spacelift_pi_key_id        = var.spacelift_api_key_id
+  spacelift_api_key_secret   = var.spacelift_api_key_secret
+  spacelift_api_key_endpoint = var.spacelift_api_key_endpoint
+  ec2_instance_type          = "t3.medium"
+  min_size                   = 1
+  max_size                   = 5
+  worker_pool_id             = var.worker_pool_id
+  security_groups            = var.worker_pool_security_groups
+  vpc_subnets                = var.worker_pool_subnets
+}
