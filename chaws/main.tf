036b76eee5050a1f95dc7dac021770d8d5dfaf79
Curtis Hodak <curtish@spacelift.com>
2025-05-08 14:02:20 -0400

    adding vpc from intermediate repo but with changes

--- chaws/main.tf
+++ chaws/main.tf
@@ -1,4 +1,6 @@
-// Now deleting the vpc, I don't use it, check approval
-# resource "aws_vpc" "example" {
-#   cidr_block = "10.0.0.0/16"
-# }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+}
+
+resource 
