fa034b1cfcffeb17b922d2ebf7a807f7e344a719
Curtis Hodak <curtish@spacelift.com>
2025-05-08 17:32:37 -0400

    fixing workers3

--- chaws/workerpool.tf
+++ chaws/workerpool.tf
@@ -9,13 +9,13 @@ module "my_workerpool" {
     export SPACELIFT_SENSITIVE_OUTPUT_UPLOAD_ENABLED=true
   EOF
 
-  spacelift_api_key_id       = var.spacelift_api_key_id
-  spacelift_api_key_secret   = var.spacelift_api_key_secret
-  spacelift_api_key_endpoint = var.spacelift_api_key_endpoint
-  ec2_instance_type          = "t3.medium"
-  min_size                   = 1
-  max_size                   = 5
-  worker_pool_id             = var.worker_pool_id
-  security_groups            = var.worker_pool_security_groups
-  vpc_subnets                = var.worker_pool_subnets
+  ec2_instance_type = "t3.medium"
+  min_size          = 1
+  max_size          = 5
+  worker_pool_id    = var.worker_pool_id
+  security_groups   = var.worker_pool_security_groups
+  vpc_subnets       = var.worker_pool_subnets
+  api_key_secret    = var.spacelift_api_key_secret
+  api_key_endpoint  = var.spacelift_api_key_endpoint
+  api_key_id        = var.spacelift_api_key_id
 }
