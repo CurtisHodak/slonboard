32fe4f8fc43fd1eb0101dab3d464ca8287f641f4
Curtis Hodak <curtish@spacelift.com>
2025-05-09 16:17:57 -0400

    fix ec2

--- compute-storage/ec2.tf
+++ compute-storage/ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "onboarding1" {
   ami                    = var.instance_ami
   instance_type          = var.instance_type
   key_name               = "chkey1"
-  vpc_security_group_ids = ["sg-08958f363d0e79c41"]
 
   root_block_device {
     volume_size = var.volume_size
