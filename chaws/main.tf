1cacb3259e37080c6a4005eac8b2b4a2ec5754a3
Curtis Hodak <curtish@spacelift.com>
2025-05-12 09:32:38 -0400

    Deleting infra for now, no need to keep things running

--- chaws/main.tf
+++ chaws/main.tf
@@ -1,128 +1,128 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+# resource "aws_vpc" "main" {
+#   cidr_block       = "10.0.0.0/16"
+#   instance_tenancy = "default"
 
-  tags = {
-    Name = "Main"
-  }
-}
-# Generate random string
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-  upper   = false
-}
-# Private Subnet
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-2a"
-  tags = {
-    Name = "private-subnet-${random_string.suffix.id}"
-  }
-}
-# Public Subnet
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-2b"
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "public-subnet-${random_string.suffix.id}"
-  }
-}
+#   tags = {
+#     Name = "Main"
+#   }
+# }
+# # Generate random string
+# resource "random_string" "suffix" {
+#   length  = 8
+#   special = false
+#   upper   = false
+# }
+# # Private Subnet
+# resource "aws_subnet" "private" {
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = "10.0.1.0/24"
+#   availability_zone = "us-east-2a"
+#   tags = {
+#     Name = "private-subnet-${random_string.suffix.id}"
+#   }
+# }
+# # Public Subnet
+# resource "aws_subnet" "public" {
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = "10.0.2.0/24"
+#   availability_zone       = "us-east-2b"
+#   map_public_ip_on_launch = false
+#   tags = {
+#     Name = "public-subnet-${random_string.suffix.id}"
+#   }
+# }
 
-# Internet Gateway
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "internet-gateway-${random_string.suffix.id}"
-  }
-}
-# Route Table for public subnet
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-  tags = {
-    Name = "public-route-table-${random_string.suffix.id}"
-  }
-}
-# Associate the public route table with the public subnet
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-# NAT Gateway
-resource "aws_eip" "nat" {
-  domain = "vpc"
-  tags = {
-    Name = "nat-eip-${random_string.suffix.id}"
-  }
-}
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
-  tags = {
-    Name = "nat-gateway-${random_string.suffix.id}"
-  }
-}
-# Route Table for private subnet
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main.id
-  }
-  tags = {
-    Name = "private-route-table-${random_string.suffix.id}"
-  }
-}
-# Associate the private route table with the private subnet
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
-}
-# Security Group for the worker pool
-resource "aws_security_group" "main" {
-  name        = "worker-pool-example-${random_string.suffix.id}"
-  description = "Worker pool security group, with unrestricted egress and restricted ingress"
-  vpc_id      = aws_vpc.main.id
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-# Security Group Ingress Rules
-resource "aws_security_group_rule" "ingress_rule_1" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["3.248.70.24/32"]
-  description       = "Allow ingress from Spacelift IP 3.248.70.24"
-  security_group_id = aws_security_group.main.id
-}
-resource "aws_security_group_rule" "ingress_rule_2" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["34.246.213.75/32"]
-  description       = "Allow ingress from Spacelift IP 34.246.213.75"
-  security_group_id = aws_security_group.main.id
-}
-resource "aws_security_group_rule" "ingress_rule_3" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["52.49.218.181/32"]
-  description       = "Allow ingress from Spacelift IP 52.49.218.181"
-  security_group_id = aws_security_group.main.id
-}
+# # Internet Gateway
+# resource "aws_internet_gateway" "main" {
+#   vpc_id = aws_vpc.main.id
+#   tags = {
+#     Name = "internet-gateway-${random_string.suffix.id}"
+#   }
+# }
+# # Route Table for public subnet
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.main.id
+#   }
+#   tags = {
+#     Name = "public-route-table-${random_string.suffix.id}"
+#   }
+# }
+# # Associate the public route table with the public subnet
+# resource "aws_route_table_association" "public" {
+#   subnet_id      = aws_subnet.public.id
+#   route_table_id = aws_route_table.public.id
+# }
+# # NAT Gateway
+# resource "aws_eip" "nat" {
+#   domain = "vpc"
+#   tags = {
+#     Name = "nat-eip-${random_string.suffix.id}"
+#   }
+# }
+# resource "aws_nat_gateway" "main" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.public.id
+#   tags = {
+#     Name = "nat-gateway-${random_string.suffix.id}"
+#   }
+# }
+# # Route Table for private subnet
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.main.id
+#   }
+#   tags = {
+#     Name = "private-route-table-${random_string.suffix.id}"
+#   }
+# }
+# # Associate the private route table with the private subnet
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.private.id
+#   route_table_id = aws_route_table.private.id
+# }
+# # Security Group for the worker pool
+# resource "aws_security_group" "main" {
+#   name        = "worker-pool-example-${random_string.suffix.id}"
+#   description = "Worker pool security group, with unrestricted egress and restricted ingress"
+#   vpc_id      = aws_vpc.main.id
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+# }
+# # Security Group Ingress Rules
+# resource "aws_security_group_rule" "ingress_rule_1" {
+#   type              = "ingress"
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "tcp"
+#   cidr_blocks       = ["3.248.70.24/32"]
+#   description       = "Allow ingress from Spacelift IP 3.248.70.24"
+#   security_group_id = aws_security_group.main.id
+# }
+# resource "aws_security_group_rule" "ingress_rule_2" {
+#   type              = "ingress"
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "tcp"
+#   cidr_blocks       = ["34.246.213.75/32"]
+#   description       = "Allow ingress from Spacelift IP 34.246.213.75"
+#   security_group_id = aws_security_group.main.id
+# }
+# resource "aws_security_group_rule" "ingress_rule_3" {
+#   type              = "ingress"
+#   from_port         = 443
+#   to_port           = 443
+#   protocol          = "tcp"
+#   cidr_blocks       = ["52.49.218.181/32"]
+#   description       = "Allow ingress from Spacelift IP 52.49.218.181"
+#   security_group_id = aws_security_group.main.id
+# }
