67a26065298ed7180e7186e0cdd75c523c47ac37
Curtis Hodak <curtish@spacelift.com>
2025-05-13 10:06:05 -0400

    testing with stacks

--- admin/main.tf
+++ admin/main.tf
@@ -13,6 +13,22 @@ resource "spacelift_stack" "main" {
   github_action_deploy             = false
 }
 
+resource "spacelift_stack" "compute-storage" {
+  name     = "compute-storage"
+  space_id = "root"
+
+  repository   = "slonboard"
+  branch       = "main"
+  project_root = "compute-storage"
+
+  terraform_version                = "1.5.7"
+  terraform_workflow_tool          = "TERRAFORM_FOSS"
+  terraform_smart_sanitization     = true
+  enable_well_known_secret_masking = true
+  github_action_deploy             = false
+}
+
+
 resource "spacelift_context_attachment" "firstcontext" {
   context_id = "firstcontext"
   stack_id   = spacelift_stack.main.id
