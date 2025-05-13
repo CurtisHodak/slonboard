9fb8efafd38e45225c0c47b30799158b435fbba2
Curtis Hodak <curtish@spacelift.com>
2025-05-13 09:56:59 -0400

    fix variables

--- admin/variables.tf
+++ admin/variables.tf
@@ -1,10 +1,10 @@
-variable "api_key_id" {
+variable "spacelift_api_key_id" {
   type        = string
   description = "api key id for spacelift"
   default     = ""
 }
 
-variable "api_key_secret" {
+variable "spacelift_api_key_secret" {
   type        = string
   description = "api key secret for spacelift"
   default     = ""
