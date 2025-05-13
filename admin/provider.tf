5afa37ff8e034872428af349a1dbba986b49b0a4
Curtis Hodak <curtish@spacelift.com>
2025-05-13 09:57:45 -0400

    fix variables

--- admin/provider.tf
+++ admin/provider.tf
@@ -1,7 +1,7 @@
 provider "spacelift" {
   api_key_endpoint = "https://curtishodak.app.spacelift.io"
   api_key_id       = var.spacelift_api_key_id
-  api_key_secret   = var.spacelift_spi_key_secret
+  api_key_secret   = var.spacelift_api_key_secret
 }
 
 terraform {
