66c1e6030f8dd213831542a4646a46a9843b9513
Curtis Hodak <curtish@spacelift.com>
2025-05-13 09:52:31 -0400

    fixing some more of admin

--- admin/provider.tf
+++ admin/provider.tf
@@ -1,4 +1,8 @@
-provider "spacelift" {}
+provider "spacelift" {
+  api_key_endpoint = "https://curtishodak.app.spacelift.io"
+  api_key_id       = var.spacelift_first_key_id
+  api_key_secret   = var.spacelift_first_key_secret
+}
 
 terraform {
   required_providers {
