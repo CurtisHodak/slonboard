fd803afd6aa1c66c19f91dc16fdf81b2e5d67ccf
Curtis Hodak <curtish@spacelift.com>
2025-05-13 09:52:01 -0400

    fix admin

--- admin/main.tf
+++ admin/main.tf
@@ -1,19 +1,19 @@
 resource "spacelift_stack" "main" {
-   name = "main"
-   space_id = "root"
-   
-   repository = "slonbaord"
-   branch = "main"
-   project_root = "chaws"
+  name     = "main"
+  space_id = "root"
 
-   terraform_version = "1.5.7"
-   terraform_workflow_tool = "TERRAFORM_FOSS"
-   terraform_smart_sanitization = true
-   enable_well_known_secret_masking = true
-   github_action_deploy = false
+  repository   = "slonbaord"
+  branch       = "main"
+  project_root = "chaws"
+
+  terraform_version                = "1.5.7"
+  terraform_workflow_tool          = "TERRAFORM_FOSS"
+  terraform_smart_sanitization     = true
+  enable_well_known_secret_masking = true
+  github_action_deploy             = false
 }
 
 resource "spacelift_context_attachment" "firstcontext" {
   context_id = "firstcontext"
-  stack_id = spacelift_stack.main.id
+  stack_id   = spacelift_stack.main.id
 }
---
+++ admin/variables.tf
@@ -0,0 +1,11 @@
+variable "api_key_id" {
+  type        = string
+  description = "api key id for spacelift"
+  default     = ""
+}
+
+variable "api_key_secret" {
+  type        = string
+  description = "api key secret for spacelift"
+  default     = ""
+}
