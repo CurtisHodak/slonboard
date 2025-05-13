0bc91a3c7e556194278c798a346f10facf3add2a
Curtis Hodak <curtish@spacelift.com>
2025-05-13 09:41:10 -0400

    testing stack creation again

---
+++ admin/main.tf
@@ -0,0 +1,20 @@
+resource "spacelift_stack" "main" {
+   name = "main"
+   space_id = "root"
+
+   namespace = "spacelift-io"
+   repository = "slonbaord"
+   branch = "main"
+   project_root = "chaws"
+
+   terraform_version = "1.5.7"
+   terraform_workflow_tool = "TERRAFORM_FOSS"
+   terraform_smart_sanitization = true
+   enable_well_known_secret_masking = true
+   github_action_deploy = false
+}
+
+resource "spacelift_context_attachment" "firstcontext" {
+  context_id = "firstcontext"
+  stack_id = spacelift_stack.main.id
+}
