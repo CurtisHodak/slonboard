f49dc0f762effbde0513e9bb03b521bf995e6599
Curtis Hodak <curtish@spacelift.com>
2025-05-23 11:26:08 -0400

    removing running infra

--- dev/ec2.tf
+++ dev/ec2.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "dev1a" {
-  ami                    = var.instance_ami
-  instance_type          = var.instance_type
-  key_name               = "chkey1"
+# resource "aws_instance" "dev1a" {
+#   ami                    = var.instance_ami
+#   instance_type          = var.instance_type
+#   key_name               = "chkey1"
 
-  root_block_device {
-    volume_size = var.volume_size
-  }
+#   root_block_device {
+#     volume_size = var.volume_size
+#   }
 
-  tags = {
-    name = "dev1a"
-    test = "New Changes1"
-  }
-}
+#   tags = {
+#     name = "dev1a"
+#     test = "New Changes1"
+#   }
+# }
