e05da26489bc21330790611a310c1b0622842c37
Curtis Hodak <curtish@spacelift.com>
2025-06-06 15:26:02 -0400

    delete

--- admin/main.tf
+++ admin/main.tf
@@ -13,20 +13,20 @@ resource "spacelift_stack" "main" {
   github_action_deploy             = false
 }
 
-resource "spacelift_stack" "compute-storage" {
-  name     = "compute-storage"
-  space_id = "root"
+# resource "spacelift_stack" "compute-storage" {
+#   name     = "compute-storage"
+#   space_id = "root"
 
-  repository   = "slonboard"
-  branch       = "main"
-  project_root = "compute-storage"
+#   repository   = "slonboard"
+#   branch       = "main"
+#   project_root = "compute-storage"
 
-  terraform_version                = "1.5.7"
-  terraform_workflow_tool          = "TERRAFORM_FOSS"
-  terraform_smart_sanitization     = true
-  enable_well_known_secret_masking = true
-  github_action_deploy             = false
-}
+#   terraform_version                = "1.5.7"
+#   terraform_workflow_tool          = "TERRAFORM_FOSS"
+#   terraform_smart_sanitization     = true
+#   enable_well_known_secret_masking = true
+#   github_action_deploy             = false
+# }
 
 resource "spacelift_stack" "database" {
   name     = "database"
