a0d9d141df82cd2d7cc0ee07e4d535f3d0e52d77
Curtis Hodak <curtish@spacelift.com>
2025-07-03 15:05:59 -0400

    Add s3 bucket

--- main.tf
+++ main.tf
@@ -182,5 +182,18 @@
 #   security_groups   = [aws_security_group.main.id]
 #   vpc_subnets       = [aws_subnet.public.id, aws_subnet.private.id]
 # }
+#####
 
-##
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "testingbuckettestch12332"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
