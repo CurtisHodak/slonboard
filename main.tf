a320789fb91a0cc04e81eb4155121026aa33b43f
Curtis Hodak <curtish@spacelift.com>
2025-11-20 10:44:57 -0500

    remove instances that don't matter

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-# resource "aws_instance" "onboarding1" {
-#   ami           = var.instance_ami
-#   instance_type = var.instance_type
-#   key_name      = "chkey1"
-
-#   root_block_device {
-#     volume_size = 20
-#   }
-
-#   tags = {
-#     test = "New Changes123"
-#   }
-# }
-
-# resource "aws_instance" "onboarding2" {
-#    ami           = var.instance_ami
-#    instance_type = var.instance_type
-#    key_name      = "chkey1"
-
-#    root_block_device {
-#      volume_size = 20
-#    }
-
-#   tags = {
-#     test = "New Changes1"
-#   }
-# }
-
 # resource "aws_iam_user" "lb" {
 #   name = "loadbalancer"
 #   path = "/system/"
